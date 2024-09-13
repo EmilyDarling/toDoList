45e5aad27f4a3f71cb033726901a82e9325c4836
Angelina <angelaaquin13@gmail.com>
2024-09-13 21:44:06 +0000

    completed with basic styling

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 //number total items on list
 //style
 
+
 	 
 
 const Home = () => {
@@ -23,24 +24,34 @@ const Home = () => {
 		 
 		 setList (newList);
 	}
+
+	function enterFunc(event) {
+		let mykey = event.key;
+		if (mykey == "Enter") { 
+			setList([...list, [task]]);
+		}
+	  }
+
 	return (
-	<>
-		<div className="text-center">
-			<input type="text" onChange= {(e)=> setTask(e.target.value)} />
-			<button onClick= {()=> setList([...list, [task]])}>Add to list</button>
+	<body className="mt-5">
+		<h2 className="text-center ">My Todos</h2>
+		<div className="inputLine text-center mb-2">
+			<input className="bg-light border border-light" id="taskInput" type="text" onChange= {(e)=> setTask(e.target.value)}  onKeyDown={(Event)=> enterFunc(Event)} placeholder="What do you need to do?"/>
+			
 		</div>
 		<div className="text-center">
-			<ul className="list-inline">
+			<ul className="text-center p-0" >
 				{list.map((entry,index) =>
-					{return <li key={index} className="bg-light"> {entry}
-					<button onClick= {()=> removeTask(index)} className="ms-3 btn text-center badge rounded-pill bg-secondary">X</button>
+					{return <li key={index} className="bg-light border border-secondary"> {entry}
+					<button onClick= {()=> removeTask(index)} className="ms-3 btn text-center badge rounded-pill bg-secondary xBtn">X</button>
 					</li>}
 					)
 				}
+				<li>{list.length} items left</li>
 			</ul>
 		</div>
 		
-		</>
+		</body>
 	);
 };
 
